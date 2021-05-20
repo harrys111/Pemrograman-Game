81566759c02341e8c35cb0c8eac3497c20dfa811
Xcaliose <72207511+harrys111@users.noreply.github.com>
2021-05-20 08:53:00 +0700

    Score

---
+++ Assets/Scripts/ScoreRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreRenderer : MonoBehaviour
+{
+    public HealthScript player;
+
+    void Update()
+    {
+        GetComponent<Text>().text = "Score: " + player.health.ToString(); 
+    }
+}
